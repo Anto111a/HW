c0cd91ce0d3847193f22d971f367dee4133a70f4
Anto111a <gotvjanskii@gmail.com>
2020-09-02 15:30:14 +0300

    add lecture_2/task_1.js

---
+++ lecture_2/task_1.js
@@ -0,0 +1,20 @@
+var myGlobal = 10;
+
+function fun1() {
+  oopsGlobal=5;
+}
+
+fun1();
+fun2();
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  
+  console.log(output);
+}
