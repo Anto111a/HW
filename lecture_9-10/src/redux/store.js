9fa37e714b370ea350944cac0f62aa52f8ee55c5
Anto111a <gotvjanskii@gmail.com>
2020-10-06 00:59:25 +0300

    fix Redux devtools

--- lecture_9-10/src/redux/store.js
+++ lecture_9-10/src/redux/store.js
@@ -2,11 +2,12 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from './rootReducer.js';
 
-const middlewareEnhancer = applyMiddleware(thunk);
+const dev = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'local';
+
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = dev && devtools ? devtools : compose;
 
 export const store = createStore(
   rootReducer,
-  compose(
-    middlewareEnhancer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+  composeEnhancers(applyMiddleware(thunk))
+);
